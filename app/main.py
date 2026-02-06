f418bc3a85e827ba6a5bd5b8fa194a9d3da72541
Jonys28 <66275854+Jonys28@users.noreply.github.com>
2026-02-06 20:25:05 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,10 @@
 def count_occurrences(phrase: str, letter: str) -> int:
     # write your code here
-    pass
+    count = 0
+    phras = phrase.lower()
+    for i in range(len(phras)-1):
+        if phras[i] == phras[i+1]:
+            l = phras[i]
+            count += 1
+    return f"The letter {l} is {count} keer in phrase"
+            
